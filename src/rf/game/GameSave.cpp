d484f2cbee0239cbef42a1d4ca5c97616fddef35
David Petrizze <dpetrizze@gmail.com>
2017-09-14 09:01:13 -0600

    Prettier world generation

--- src/rf/game/GameSave.cpp
+++ src/rf/game/GameSave.cpp
@@ -24,7 +24,7 @@ namespace rf {
     Level GameSave::level(Id id) {
       assert(id == 1);
 
-      Vec2u level_size(32, 32);
+      Vec2u level_size(30, 30);
 
       Level lv;
       lv.tiles.resize(level_size);
@@ -48,17 +48,80 @@ namespace rf {
 
       lv.objects[1].set_playable(true);
 
+      Color bg_color(0x11, 0x22, 0x22);
+
       for(unsigned int j = 0 ; j < level_size.y ; j ++) {
         for(unsigned int i = 0 ; i < level_size.x ; i ++) {
           Vec2u pi(i, j);
           auto & tile = lv.tiles.get(pi);
-          unsigned int r = (rand() % 100);
-          if(r <= 8) {
-            tile.add(new BasicTileGlyph(Glyph(5 + 7*16, Color(0x33, 0x66, 0x33))));
-          } else {
-            tile.add(new BasicTileGlyph(Glyph(5 + 6*16, Color(0x22, 0x44, 0x44))));
-          }
+          tile.add(new BasicTileGlyph(
+            Glyph(4, bg_color, bg_color)
+          ));
         }
+      }
+
+      for(unsigned int j = 0 ; j < 10 ; j ++) {
+        Vec2i drunkard((rand() % 16) + 8, (rand() % 16) + 8);
+        for(unsigned int i = 0 ; i < 50 ; i ++) {
+          drunkard.x += (rand() % 3) - 1;
+          drunkard.y += (rand() % 3) - 1;
+
+          if(drunkard.x < 0) { drunkard.x = 0; }
+          if(drunkard.x > level_size.x - 1) { drunkard.x = level_size.x - 1; }
+          if(drunkard.y < 0) { drunkard.y = 0; }
+          if(drunkard.y > level_size.y - 1) { drunkard.y = level_size.y - 1; }
+
+          auto & tile = lv.tiles[drunkard];
+          tile = Tile();
+          tile.add(new BasicTileGlyph(
+            Glyph(
+              5 + ((rand() % 2) + 6)*16,
+              Color(0x88, 0x66, 0x44),
+              bg_color
+            )
+          ));
+        }
+      }
+
+      int min_height = 1;
+      int height = 2;
+      int max_height = 4;
+      for(unsigned int x = 0 ; x < level_size.x ; x ++) {
+        for(int y = 0 ; y < height ; y ++) {
+          Vec2u pi(x, y);
+          auto & tile = lv.tiles[pi];
+          tile = Tile();
+          tile.add(new BasicTileGlyph(
+            Glyph(
+              (rand() % 3) + 5 + 4*16,
+              y >= height - 2 ? Color(0x33, 0x66, 0x33) : Color(0x22, 0x44, 0x44),
+              bg_color
+            )
+          ));
+        }
+        height += (rand() % 3) - 1;
+        if(height < min_height) { height = min_height; }
+        if(height > max_height) { height = max_height; }
+      }
+
+      height = 1;
+      //int max_height = 2;
+      for(unsigned int x = 0 ; x < level_size.x ; x ++) {
+        for(int y = level_size.y - height ; y < level_size.y ; y ++) {
+          Vec2u pi(x, y);
+          auto & tile = lv.tiles[pi];
+          tile = Tile();
+          tile.add(new BasicTileGlyph(
+            Glyph(
+              (rand() % 3) + 8 + 3*16,
+              Color(0x88, 0x66, 0x44),
+              bg_color
+            )
+          ));
+        }
+        height += (rand() % 3) - 1;
+        if(height < min_height) { height = min_height; }
+        if(height > max_height) { height = max_height; }
       }
 
       return lv;
